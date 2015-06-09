a5712a79905b6e98688736653d8c9172c92fb4be
Alexey Kolmakov <alexey.kolmakov@starcode.ru>
2015-06-09 08:57:30 +0300

    Add model for contact

---
+++ models/Contact.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
+
+/**
+ * This is the model class for table "contact".
+ *
+ * @property integer $id
+ * @property string $phone
+ * @property string $name
+ * @property string $updated
+ * @property string $created
+ */
+class Contact extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%contact}}';
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class'              => TimestampBehavior::className(),
+                'createdAtAttribute' => 'created',
+                'updatedAtAttribute' => 'updated',
+                'value'              => new Expression('NOW()'),
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['phone', 'name'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id'      => 'ID',
+            'phone'   => 'Phone',
+            'name'    => 'Name',
+            'updated' => 'Updated',
+            'created' => 'Created',
+        ];
+    }
+}
